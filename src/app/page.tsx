04fb9f139e940afb767b08f28b10fbbcfde88cb4
LanhNguyen <lanhbmt12041997@gmail.com>
2025-03-13 08:20:17 +0700

    feat: combine components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import FavoritePlaces from '@/components/organisms/FavoritePlaces'
+import IntroductionSection from '@/components/organisms/IntroductionSection'
 import '../styles/global.scss'
 
 export default function Home() {
-  return <div>Hello World</div>
+  return (
+    <>
+      <IntroductionSection />
+      <FavoritePlaces />
+    </>
+  )
 }
